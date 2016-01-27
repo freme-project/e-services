7f28e84f420eeb129beed50133a25338368ccd69
Arne Binder <arne.b.binder@gmail.com>
2016-01-27 15:22:26 +0100

    minor changes

--- linking/src/test/java/eu/freme/eservices/linking/LinkingControllerTest.java
+++ linking/src/test/java/eu/freme/eservices/linking/LinkingControllerTest.java
@@ -4,27 +4,34 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import eu.freme.bservices.testhelper.AuthenticatedTestHelper;
 import eu.freme.bservices.testhelper.OwnedResourceManagingHelper;
 import eu.freme.bservices.testhelper.SimpleEntityRequest;
+import eu.freme.bservices.testhelper.ValidationHelper;
 import eu.freme.bservices.testhelper.api.IntegrationTestSetup;
+import eu.freme.common.conversion.rdf.RDFConstants;
 import eu.freme.common.persistence.model.OwnedResource;
 import eu.freme.common.persistence.model.Template;
 import org.apache.log4j.Logger;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
 /**
  * Created by Arne Binder (arne.b.binder@gmail.com) on 27.01.2016.
  */
 public class LinkingControllerTest {
     private Logger logger = Logger.getLogger(LinkingControllerTest.class);
     private AuthenticatedTestHelper ath;
+    private ValidationHelper vh;
     private OwnedResourceManagingHelper<Template> ormh;
+
     final static String serviceUrl = "/e-link";
     final static String mockupUrl = "/mockups/file";
     final static String mockupEntityUrl = "/linking-ELINK.ttl";
@@ -32,6 +39,7 @@ public class LinkingControllerTest {
     public LinkingControllerTest() throws UnirestException {
         ApplicationContext context = IntegrationTestSetup.getContext("linking-controller-test-package.xml");
         ath = context.getBean(AuthenticatedTestHelper.class);
+        vh = context.getBean(ValidationHelper.class);
         ormh = new OwnedResourceManagingHelper<>(serviceUrl,Template.class, ath, null);
         ath.authenticateUsers();
     }
@@ -65,15 +73,15 @@ public class LinkingControllerTest {
     }
 
     @Test
-    public void TestELinkExploreSparqlMockup() throws UnirestException, IOException {
+    public void testELinkExploreSparqlMockup() throws UnirestException, IOException {
         /*HttpResponse<String> response;
 
         String rdf_resource = "http://dbpedia.org/resource/Berlin";
-        String endpoint = baseUrl+"/mockups/file/EXPLORE-Berlin.ttl";
+        String endpoint = ath.getAPIBaseUrl()+ mockupUrl +"/linking-EXPLORE-Berlin.ttl";
 
         rdf_resource = "http://dbpedia.org/resource/Berlin";
 
-        response= post("/explore")
+        response= Unirest.post(ath.getAPIBaseUrl()+serviceUrl+"/explore")
                 .queryString("informat","turtle")
                 .queryString("outformat","turtle")
                 .queryString("endpoint-type","sparql")
@@ -81,17 +89,24 @@ public class LinkingControllerTest {
                 .queryString("endpoint", endpoint)
                 .asString();
 
-        validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
-        */
+        vh.validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
+*/
     }
 
+    /*@Test
+    public void testDummy() throws UnirestException {
+        HttpResponse<String> response =  Unirest.get(ath.getAPIBaseUrl()+mockupUrl+"/linking-EXPLORE-ldf-resource-Berlin.ttl").asString();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }*/
+
     @Test
-    public void TestELinkExploreLdfMockup() throws UnirestException, IOException {
+    public void testELinkExploreLdfMockup() throws UnirestException, IOException {
 /*
         String rdf_resource = "http://dbpedia.org/resource/Berlin";
-        String endpoint = baseUrl+"/mockups/file/EXPLORE-ldf-resource-Berlin.ttl";
+        String endpoint = ath.getAPIBaseUrl()+ mockupUrl + "/linking-EXPLORE-ldf-resource-Berlin.ttl";
+        endpoint = endpoint.replace("localhost", "127.0.0.1");
 
-        HttpResponse<String> response = post("/explore")
+        HttpResponse<String> response =  Unirest.post(ath.getAPIBaseUrl()+serviceUrl+"/explore")
                 .queryString("informat","turtle")
                 .queryString("outformat","turtle")
                 .queryString("endpoint-type","ldf")
@@ -99,7 +114,8 @@ public class LinkingControllerTest {
                 .queryString("endpoint", endpoint)
                 .asString();
 
-        validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
+
+        vh.validateNIFResponse(response, RDFConstants.RDFSerialization.TURTLE);
 */
     }
 
