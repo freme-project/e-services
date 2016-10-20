cd0f22d8441e4a0eecd0a71ccaa978f614a0013f
bgrusdt <grusdt@uni-potsdam.de>
2016-10-20 15:02:08 +0200

    added license headers

--- publishing/src/main/java/eu/freme/eservices/publishing/webservice/Person.java
+++ publishing/src/main/java/eu/freme/eservices/publishing/webservice/Person.java
@@ -1,13 +1,16 @@
 /**
- * Copyright (C) 2015 Agro-Know, Deutsches Forschungszentrum für Künstliche Intelligenz, iMinds,
- * Institut für Angewandte Informatik e. V. an der Universität Leipzig,
- * Istituto Superiore Mario Boella, Tilde, Vistatec, WRIPL (http://freme-project.eu)
+ * Copyright © 2016 Agro-Know, Deutsches Forschungszentrum für Künstliche
+ * 							Intelligenz, iMinds,
+ * 							Institut für Angewandte Informatik e. V. an
+ * 							der Universität Leipzig,
+ * 							Istituto Superiore Mario Boella, Tilde,
+ * 							Vistatec, WRIPL (http://freme-project.eu)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
